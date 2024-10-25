6b3275cd43e2d525124d6d81e2f6d268fa81cb6f
Bosy-Ayman <112729185+Bosy-Ayman@users.noreply.github.com>
2024-10-25 22:06:25 +0300

    remove

--- searching/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    Search the deepest nodes in the search tree first.
-    Search through the successors of a problem to find a goal.
-    The argument frontier should be an empty stack.
-    Repeats infinitely in case of loops.
-    """
