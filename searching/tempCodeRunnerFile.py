2907b2b2c0a9baf52eb1cda66ee4d6484d46b8e4
Bosy-Ayman <112729185+Bosy-Ayman@users.noreply.github.com>
2024-10-25 22:05:06 +0300

    add

---
+++ searching/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+    Search the deepest nodes in the search tree first.
+    Search through the successors of a problem to find a goal.
+    The argument frontier should be an empty stack.
+    Repeats infinitely in case of loops.
+    """
