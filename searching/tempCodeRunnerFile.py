b8a336d570203a8bcafb41ac373481df024c0609
Bosy-Ayman <112729185+Bosy-Ayman@users.noreply.github.com>
2024-11-15 15:27:46 +0200

    edit IDS

--- searching/tempCodeRunnerFile.py
+++ searching/tempCodeRunnerFile.py
@@ -1,2 +1,7 @@
-with open('csvs_and_jsons\\random_users.json', 'r') as file:
-    data = json.load(file)
+# Define a Node class to encapsulate state, cost, depth, and parent information
+class Node:
+    def __init__(self, state, parent=None, cost=0, depth=0):
+        self.state = state
+        self.parent = parent
+        self.cost = cost
+        self.depth = depth
