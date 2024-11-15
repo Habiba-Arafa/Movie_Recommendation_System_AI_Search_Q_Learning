3aab6165b18ff09e6385933d781532d49366c21c
shahdhoss <s-shahd.fouda@zewailcity.edu.eg>
2024-11-15 17:46:54 +0200

    bfs visualiation

--- searching/bfs.py
+++ searching/bfs.py
@@ -3,25 +3,44 @@ import json
 from problem_modeling import Node
 from collections import deque
 import time
+from pyvis.network import Network
+
 
 with open('csvs_and_jsons\\random_users.json', 'r') as file:
     data = json.load(file)
 
+with open('csvs_and_jsons\\movie_vectors.json', 'r') as file:
+    movies = json.load(file)
+
+
 def breadth_first_tree_search(problem):
     frontier = deque([Node.root(problem.initial_state)])
+    net = Network(notebook=True, height="600px", width="100%")  
     while frontier:
         node = frontier.popleft()  # Used as a queue
         state_tuple = tuple(node.state)
         if state_tuple in problem.visited:
             continue
         problem.visited.add(state_tuple)
+        movie_name = ''
+        for name, vector in movies.items():
+            if vector == list(state_tuple):
+                movie_name = name
+                break
+        net.add_node(str(state_tuple), label=movie_name, color="lightblue")
+        if node.parent:
+            parent_state = tuple(node.parent.state)
+            net.add_node(str(parent_state), label=str(parent_state), color="lightgreen")
+            net.add_edge(str(parent_state), str(state_tuple))
         recommended_movie, cost = problem.goal_test(node)
         if recommended_movie and cost:
+            net.show("bfs_tree.html")  
             return recommended_movie, cost
         for movie_index in range(len(problem.initial_state)):
             new_state = problem.actions(node.state, movie_index)
             child_node = Node.child(problem, node, new_state)
             frontier.append(child_node)
+    net.show("bfs_tree.html")  # Visualize and save the tree as an HTML file
     return None
 
 user_id = 'user4'
